3f44fd903c07e88809ea665bf99c993f518afef2
Shipra <shipratripathi.500@gmail.com>
2023-07-21 16:07:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let cel = document.getElementById("cel");
+let fah = document.getElementById("fah");
+
+function fun() {
+    let c=cel.value;
+            let f= (c * 9/5) + 32;
+            if(!Number.isInteger(f)){
+                f=f.toFixed(4);
+            }
+            fah.value=f;
+}
+
+// cel.addEventListener("input" , function(){
+//     let c = this.value;
+//     let f = (c * 9/5) + 32;
+
+//     fah.value = f;
+// })
+
+// fah.addEventListener("input" , function(){
+//     let f = this.value;
+//     let c = (f - 32) *  5/9 ;
+
+//     if (!Number.isInteger(c)) {
+//         c = f.toFixed(4);
+//     }
+
+//     cel.value = c;
+// })
